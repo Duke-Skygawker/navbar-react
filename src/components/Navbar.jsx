d1ade78030a245dfd6e23293e3174788e083811b
Duke-Skygawker <bart3kk990@gmail.com>
2023-12-13 16:26:04 +0100

    dynamic toggler

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { links, social } from "../data";
+import { FaBars } from "react-icons/fa";
 import Logo from "./Logo";
 import Links from "./Links";
 import Socials from "./Socials";
@@ -7,14 +8,29 @@ import Socials from "./Socials";
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
 
+  const expandLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav">
         <Logo />
-        <div className="links-container">
+        <div
+          className={
+            showLinks ? "links-container display-flex" : "links-container"
+          }
+          style={showLinks ? { height: "15rem" } : { height: "0" }}
+        >
           <Links links={links} />
           <Socials socials={social} />
         </div>
+        <button
+          className={showLinks ? "btn toggler deg90" : "btn toggler"}
+          onClick={expandLinks}
+        >
+          <FaBars />
+        </button>
       </div>
     </nav>
   );
